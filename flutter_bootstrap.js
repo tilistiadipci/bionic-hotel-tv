19f438a7c2397bd6e1416d0832c73e65b31cb534
PCI-Rey <reynaldipci@gmail.com>
2026-09-23 11:45:46 +0700

    fix: make layout 100 percent full screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "163610196" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4113588295" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
